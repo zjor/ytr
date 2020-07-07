ff6ce6287ade4cef92bb7b85cf4238accf70481e
Sergey Royz <zjor.se@gmail.com>
2020-07-07 11:52:44 +0200

    tasks added

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Command line tool for translating foreign words and keeping history for future l
 ## Requirements
 
 `YATR_KEY` environment variable with an API key should be set
+
+
+## Tasks
+- store translation history in a remote publicly available storage
+- show last translations in a default chrome page
+- enrich translations with an example and an image
+- collect translation attempts from the browser
--- ytr.py
+++ ytr.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
+import click
+
 from settings import YATR_KEY
 from ytr_client import YtrClient
 
 client = YtrClient(YATR_KEY)
 
+@click.command()
+@click.argument('text')
+def main(text):
+	ts = client.translate(text)
+	if len(ts) > 0:
+		print(f"{text} - {ts[0]}")
+	else:
+		print("No translation available")
 
-print(client.translate("hello world"))
+
+if __name__ == "__main__":
+	main()
