c523e804c417e836ec6e3977b929f74d9e66b997
Sergey Royz <zjor.se@gmail.com>
2020-07-07 11:27:50 +0200

    client

--- ytr.py
+++ ytr.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+import requests as rq
+
 from settings import YATR_KEY
 
-print(YATR_KEY)
+
+def get_languages():
+	res = rq.get("https://translate.yandex.net/api/v1.5/tr.json/getLangs", {"key": YATR_KEY})
+	return res.json()
+
+
+def translate(text, lang="en-ru"):
+	res = rq.get("https://translate.yandex.net/api/v1.5/tr.json/translate", {"key": YATR_KEY, "text": text, "lang": lang})
+	return res.json()["text"]
+
+print(translate("hello world"))
